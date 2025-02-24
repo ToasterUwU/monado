8731b6dbde28a6de3eed2ec97445bbe3d19aae49
averyv <avery@averyv.me>
2025-02-24 23:30:33 +0000

    xrt: Add brightness control feature and callbacks
    Allowing a headset's display brightness to be queried and updated.
    
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2426>

--- src/xrt/include/xrt/xrt_device.h
+++ src/xrt/include/xrt/xrt_device.h
@@ -264,6 +264,7 @@ struct xrt_device_supported
 	bool face_tracking;
 	bool body_tracking;
 	bool battery_status;
+	bool brightness_control;
 
 	bool planes;
 	enum xrt_plane_detection_capability_flags_ext plane_capability_flags;
@@ -627,6 +628,27 @@ struct xrt_device
 	                                   float *out_charge);
 
 	/*!
+	 * @brief Get the current display brightness.
+	 *
+	 * @param[in] xdev             The device.
+	 * @param[out] out_brightness  Current display brightness. Usually between 0 and 1. Some devices may
+	 *                             exceed 1 if the supported range exceeds 100%
+	 */
+	xrt_result_t (*get_brightness)(struct xrt_device *xdev, float *out_brightness);
+
+	/*!
+	 * @brief Set the display brightness.
+	 *
+	 * @param[in] xdev            The device.
+	 * @param[in] brightness      Desired display brightness. Usually between 0 and 1. Some devices may
+	 *                            allow exceeding 1 if the supported range exceeds 100%, but it will be clamped to
+	 *                            the supported range.
+	 * @param[in] relative        Whether to add \a brightness to the current brightness, instead of overwriting
+	 *                            the current brightness.
+	 */
+	xrt_result_t (*set_brightness)(struct xrt_device *xdev, float brightness, bool relative);
+
+	/*!
 	 * Enable the feature for this device.
 	 *
 	 * @param[in] xdev        The device.
@@ -924,6 +946,30 @@ static inline xrt_result_t
 xrt_device_get_battery_status(struct xrt_device *xdev, bool *out_present, bool *out_charging, float *out_charge)
 {
 	return xdev->get_battery_status(xdev, out_present, out_charging, out_charge);
+}
+
+/*!
+ * Helper function for @ref xrt_device::get_brightness.
+ *
+ * @copydoc xrt_device::get_brightness
+ * @public @memberof xrt_device
+ */
+static inline xrt_result_t
+xrt_device_get_brightness(struct xrt_device *xdev, float *out_brightness)
+{
+	return xdev->get_brightness(xdev, out_brightness);
+}
+
+/*!
+ * Helper function for @ref xrt_device::set_brightness.
+ *
+ * @copydoc xrt_device::set_brightness
+ * @public @memberof xrt_device
+ */
+static inline xrt_result_t
+xrt_device_set_brightness(struct xrt_device *xdev, float brightness, bool relative)
+{
+	return xdev->set_brightness(xdev, brightness, relative);
 }
 
 /*!
