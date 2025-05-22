542301def9c3c3dc2b96ba4c12ff5eb0c3dbaec9
Sapphire <imsapphire0@gmail.com>
2025-05-22 14:27:36 -0500

    flake: Update to NixOS 25.05, switch to flake-parts
    Part-of: <https://gitlab.freedesktop.org/monado/monado/-/merge_requests/2508>

--- flake.nix
+++ flake.nix
@@ -3,49 +3,61 @@
 
 {
   inputs = {
-    # Whenever an upstream change is merged, update this to
-    # the relevant commit and remove the packages from the
-    # ...ToUpstream lists below
-    nixpkgs.url = "github:NixOS/nixpkgs/e80d1b630036fe33badbc168dfcd071d463b92cf";
-    flake-utils.url = "github:numtide/flake-utils";
+    # Whenever an upstream change is merged, update this and
+    # remove the packages from the ...ToUpstream lists below
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-
-      devTools = with pkgs; [
-        # Tools that are required in order to develop with Monado, but that are not required to build Monado itself
-        # These cannot be upstreamed into nixpkgs, as they are not required to build Monado
-        # See https://github.com/NixOS/nix/issues/7501 for a discussion on this
-        clang
-        cmake-format
-        git
-        gradle
-        gradle-completion
+  outputs =
+    inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
+      perSystem =
+        { pkgs, lib, ... }:
+        let
+          devTools = with pkgs; [
+            # Tools that are required in order to develop with Monado
+            # but are not required to build Monado itself
+            clang-tools
+            cmake-format
+            git
+            gradle
+            gradle-completion
+          ];
 
-      nativeBuildInputsToUpstream = with pkgs; [
-        # If there are any nativeBuildInputs that are not in nixpkgs, add them here
-        # nativeBuildInputs are packages that are needed to develop and/or build the project (i.e. tooling)
-      ];
+          nativeBuildInputsToUpstream = with pkgs; [
+            # If there are any nativeBuildInputs that are not in nixpkgs, add them here
+            # nativeBuildInputs are packages that are needed to develop and/or build the project (i.e. tooling)
+          ];
 
-      buildInputsToUpstream = with pkgs; [
-        # If there are any buildInputs that are not in nixpkgs, add them here
-        # buildInputs are any packages that are needed at runtime (i.e. dependencies)
-      ];
+          buildInputsToUpstream = with pkgs; [
+            # If there are any buildInputs that are not in nixpkgs, add them here
+            # buildInputs are any packages that are needed at runtime (i.e. dependencies)
+          ];
 
-      package = pkgs.monado.overrideAttrs (oldAttrs: {
-        src = ./.;
+          package = pkgs.monado.overrideAttrs (oldAttrs: {
+            src = ./.;
 
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ nativeBuildInputsToUpstream ++ devTools;
-        buildInputs = oldAttrs.buildInputs ++ buildInputsToUpstream;
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ nativeBuildInputsToUpstream;
+            buildInputs = oldAttrs.buildInputs ++ buildInputsToUpstream;
+            cmakeFlags = lib.remove "-DXRT_HAVE_STEAM:BOOL=TRUE" oldAttrs.cmakeFlags;
 
-        patches = [];
-      });
-    in {
-      packages.default = package;
-      devShells.default = package;
-    }
-  );
+            patches = [ ];
+          });
+        in
+        {
+          packages.default = package;
+          devShells.default = package.overrideAttrs (oldAttrs: {
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ devTools;
+          });
+
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+    };
 }
